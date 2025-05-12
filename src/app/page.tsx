f275d2966ee1cf799b9806564b3db490549887a6
atulgen <atul.raj@gennextit.com>
2025-05-13 05:13:33 +0530

    export error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { HackathonCard } from "@/components/hackathon-card";
 import BlurFade from "@/components/magicui/blur-fade";
 import BlurFadeText from "@/components/magicui/blur-fade-text";
-import { ProjectCard } from "@/components/project-card";
 import { ResumeCard } from "@/components/resume-card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { DATA } from "@/data/resume";
-import Link from "next/link";
 import Markdown from "react-markdown";
-import ProjectsPage from "./projects/page";
+import ProjectsPage from "./projects/_components/ProjectPage";
 
 const BLUR_FADE_DELAY = 0.04;
 
@@ -113,7 +111,7 @@ export default function Page() {
         </div>
       </section>
 
-      <ProjectsPage isMain={true}/>
+      <ProjectsPage isMain={true} />
 
       {/* <section id="projects">
         <div className="space-y-12 w-full py-12">
