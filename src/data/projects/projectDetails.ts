84206ebae380059a1137b201cc9502dc394c1d6d
pravin-hub-rgb <pravvinkumarkumar@gmail.com>
2025-05-17 18:13:42 +0530

    update project detail + hydration error fix

--- src/data/projects/projectDetails.ts
+++ src/data/projects/projectDetails.ts
@@ -3,7 +3,7 @@ import { ProjectDetails } from "@/lib/types";
 // Project data
 export const projectData: { [key: string]: ProjectDetails } = {
   bizzlisting: {
-    slug:'bizzlisting',
+    slug: 'bizzlisting',
     name: "Bizzlisting",
     image: '/bizzlisting/image.png',
 
@@ -57,63 +57,8 @@ export const projectData: { [key: string]: ProjectDetails } = {
       }
     ],
   },
-  astrologyconsulting: {
-    slug:'astrologyconsulting',
-    name: "Astrology Consulting",
-    image: '/bizzlisting/image.png',
-
-    description:
-      "Business directory web application for local businesses to get online presence.",
-    longDescription: `
-        The web revolves around data, so in this project my major role was to create data scrapping which i did using Apify. Enhanced UI for premium looks and feel. 
-  
-        Responsive design that works seamlessly across all devicesproject showcase with detailed project pagesices and providers pages, Added SEO for better ranking,Elegant, premium and rich UI enhancements.
-      `,
-    technologies: [
-      "Next.js",
-      "React",
-      "TypeScript",
-      "Tailwind CSS",
-      "Framer Motion",
-      "Shadcn UI",
-      "Zustand",
-      "Data Scrapping",
-      "Prisma",
-      "Apify",
-      "SEO",
-    ],
-    links: {
-      repo: "",
-      live: "https://www.bizzlisting.com/Delhi",
-    },
-    dates: "December - 2025",
-    screenshots: [
-      {
-        src: "/bizzlisting/image copy.png",
-        alt: "Homepage Overview",
-        description:
-          "Clean and modern homepage layout with modern service categories and advertisements.",
-      },
-      {
-        src: "/bizzlisting/image copy 2.png",
-        alt: "Services Page",
-        description:
-          "Detailed services page with filters and service providers.",
-      },
-      {
-        src: "/bizzlisting/image copy 3.png",
-        alt: "Services Page",
-        description: "Service providers listing.",
-      },
-      {
-        src: "/bizzlisting/image copy 4.png",
-        alt: "Beautiful Footer",
-        description: "Footer with Dynamic City data.",
-      }
-    ],
-  },
   thelibrary: {
-    slug:'thelibrary',
+    slug: 'thelibrary',
     name: "The Library",
     image: '/bizzlisting/image.png',
 
@@ -123,18 +68,11 @@ export const projectData: { [key: string]: ProjectDetails } = {
         Here i have priorities on FOSS, Free and Open Source alternatives of mainstream application so that developers can easily access the source code of the tool they are using, this will further help the overall growth and web community
       `,
     technologies: [
-      "MarkDown",
       "Next.js",
-      "React",
-      "TypeScript",
-      "Tailwind CSS",
-      "Framer Motion",
-      "Shadcn UI",
-      "Zustand",
-      "Data Scrapping",
-      "Prisma",
-      "Apify",
-      "SEO",
+      "Fullstack components",
+      "Documentation",
+      "Free Software",
+      "Open Source"
     ],
     links: {
       repo: "https://codeberg.org/the-library-node",
@@ -166,6 +104,502 @@ export const projectData: { [key: string]: ProjectDetails } = {
       }
     ],
   },
+  trainersportal: {
+    slug: 'trainersportal',
+    name: "Trainer's Portal",
+    image: '/bizzlisting/image.png',
+
+    description:
+      "Technical Trainer Oriented Resume Web Application",
+    longDescription: `
+        A web application for trainers to showcase their skills and
+qualification and build their portfolio with rate cards for training
+they provide.
+      `,
+    technologies: [
+      "Next.js",
+      "Prisma",
+      "Zen Stack",
+      "Tan Stack",
+    ],
+    links: {
+      repo: "",
+      live: "https://atul.thebigocommunity.org/projects/gentrainer",
+    },
+    dates: "January 2025  ",
+    screenshots: [
+      {
+        src: "/bizzlisting/image copy.png",
+        alt: "Homepage Overview",
+        description:
+          "Clean and modern homepage layout with modern service categories and advertisements.",
+      },
+      {
+        src: "/bizzlisting/image copy 2.png",
+        alt: "Services Page",
+        description:
+          "Detailed services page with filters and service providers.",
+      },
+      {
+        src: "/bizzlisting/image copy 3.png",
+        alt: "Services Page",
+        description: "Service providers listing.",
+      },
+      {
+        src: "/bizzlisting/image copy 4.png",
+        alt: "Beautiful Footer",
+        description: "Footer with Dynamic City data.",
+      }
+    ],
+  },
+  studentattendanceportal: {
+    slug: 'studentattendanceportal',
+    name: "Student Attendance Portal",
+    image: '/bizzlisting/image.png',
+
+    description:
+      "Attendance portal for Medical students.",
+    longDescription: `
+        A Web application for MBBS institution with advance level of
+complexity regarding the layout of the organisation as students
+where segregation in complex batches structures.
+Feature added, Attendance using Face detection by saving
+student's Face Descriptors along side the student data.
+      `,
+    technologies: [
+      "Next.js",
+      "Drizzle",
+      "Database Transaction Management",
+      "Nodemailer",
+      "SMS",
+      "Tensorflow",
+      "Face Api",
+      "AI"
+    ],
+    links: {
+      repo: "",
+      live: "https://atul.thebigocommunity.org/projects/medattendance",
+    },
+    dates: "November 2024 - January 2025",
+    screenshots: [
+      {
+        src: "/bizzlisting/image copy.png",
+        alt: "Homepage Overview",
+        description:
+          "Clean and modern homepage layout with modern service categories and advertisements.",
+      },
+      {
+        src: "/bizzlisting/image copy 2.png",
+        alt: "Services Page",
+        description:
+          "Detailed services page with filters and service providers.",
+      },
+      {
+        src: "/bizzlisting/image copy 3.png",
+        alt: "Services Page",
+        description: "Service providers listing.",
+      },
+      {
+        src: "/bizzlisting/image copy 4.png",
+        alt: "Beautiful Footer",
+        description: "Footer with Dynamic City data.",
+      }
+    ],
+  },
+  astrologyconsultingfirm: {
+    slug: 'astrologyconsultingfirm',
+    name: "First Impression for Astrology Consulting Firm",
+    image: '/bizzlisting/image.png',
+
+    description:
+      "Web Application focused on showcasing the features and enrolling the visitors.",
+    longDescription: `
+        Integrate payment gateway, Creation of invoice, Sending invoice to
+Visitor
+      `,
+    technologies: [
+      "Nextjs",
+      "MongoDB",
+      "Nodemailer",
+      "Razorpay"
+    ],
+    links: {
+      repo: "",
+      live: "https://atul.thebigocommunity.org/projects/astrologyconsulting",
+    },
+    dates: "December 2024",
+    screenshots: [
+      {
+        src: "/bizzlisting/image copy.png",
+        alt: "Homepage Overview",
+        description:
+          "Clean and modern homepage layout with modern service categories and advertisements.",
+      },
+      {
+        src: "/bizzlisting/image copy 2.png",
+        alt: "Services Page",
+        description:
+          "Detailed services page with filters and service providers.",
+      },
+      {
+        src: "/bizzlisting/image copy 3.png",
+        alt: "Services Page",
+        description: "Service providers listing.",
+      },
+      {
+        src: "/bizzlisting/image copy 4.png",
+        alt: "Beautiful Footer",
+        description: "Footer with Dynamic City data.",
+      }
+    ],
+  },
+  erpforprocurementmanager: {
+    slug: 'erpforprocurementmanager',
+    name: "ERP For Procurement Manager",
+    image: '/bizzlisting/image.png',
+
+    description:
+      "FullStack Web Application in NextJS.",
+    longDescription: `
+        Creation of invoices, role based access management, Admin and
+user dashboards. This was my first hands on experience with
+complex application.
+      `,
+    technologies: [
+      "AuthJS",
+      "Tailwind",
+      "React",
+      "NextJS",
+      "Drizzle",
+      "NodeMailer",
+      "Vercel"
+    ],
+    links: {
+      repo: "",
+      live: "https://atul.thebigocommunity.org/projects/procurement",
+    },
+    dates: "September 2024",
+    screenshots: [
+      {
+        src: "/bizzlisting/image copy.png",
+        alt: "Homepage Overview",
+        description:
+          "Clean and modern homepage layout with modern service categories and advertisements.",
+      },
+      {
+        src: "/bizzlisting/image copy 2.png",
+        alt: "Services Page",
+        description:
+          "Detailed services page with filters and service providers.",
+      },
+      {
+        src: "/bizzlisting/image copy 3.png",
+        alt: "Services Page",
+        description: "Service providers listing.",
+      },
+      {
+        src: "/bizzlisting/image copy 4.png",
+        alt: "Beautiful Footer",
+        description: "Footer with Dynamic City data.",
+      }
+    ],
+  },
+  recruitmentlandingpage: {
+    slug: 'recruitmentlandingpage',
+    name: "Recruitment Landing Page",
+    image: '/bizzlisting/image.png',
+
+    description:
+      "Fullstack project from Frontend to deployement.",
+    longDescription: `
+        The project showcased my abilities in front-end development,
+integrating a Strapi headless CMS, and translating design concepts
+into a polished, responsive web application.
+      `,
+    technologies: [
+      "Nextjs",
+      "Typescript",
+      "Tailwind",
+      "AWS",
+      "Figma"
+    ],
+    links: {
+      repo: "",
+      live: "https://atul.thebigocommunity.org/projects/thethreeacross",
+    },
+    dates: "July - August 2024",
+    screenshots: [
+      {
+        src: "/bizzlisting/image copy.png",
+        alt: "Homepage Overview",
+        description:
+          "Clean and modern homepage layout with modern service categories and advertisements.",
+      },
+      {
+        src: "/bizzlisting/image copy 2.png",
+        alt: "Services Page",
+        description:
+          "Detailed services page with filters and service providers.",
+      },
+      {
+        src: "/bizzlisting/image copy 3.png",
+        alt: "Services Page",
+        description: "Service providers listing.",
+      },
+      {
+        src: "/bizzlisting/image copy 4.png",
+        alt: "Beautiful Footer",
+        description: "Footer with Dynamic City data.",
+      }
+    ],
+  },
+  thebigocommunity: {
+    slug: 'thebigocommunity',
+    name: "The Big O Community",
+    image: '/bizzlisting/image.png',
+
+    description:
+      "A community where we use and share knowledge about Free and Open Source",
+    longDescription: `
+        This project allowed me to gain hands-on experience in the full
+lifecycle of setting up an online community platform, from domain
+and hosting to the technical implementation. The content creation
+aspect also showcased my ability to research and share knowledge
+on open-source software alternatives.
+      `,
+    technologies: [
+      "Free Software",
+      "Django",
+      "Nginx",
+      "SSL",
+      "Unix Commands"
+    ],
+    links: {
+      repo: "",
+      live: "https://the-big-o-community-atulyaraj-0c6437664fef0742342132dc86cdc7377.gitlab.io/",
+    },
+    dates: "June 2023 - Present",
+    screenshots: [
+      {
+        src: "/bizzlisting/image copy.png",
+        alt: "Homepage Overview",
+        description:
+          "Clean and modern homepage layout with modern service categories and advertisements.",
+      },
+      {
+        src: "/bizzlisting/image copy 2.png",
+        alt: "Services Page",
+        description:
+          "Detailed services page with filters and service providers.",
+      },
+      {
+        src: "/bizzlisting/image copy 3.png",
+        alt: "Services Page",
+        description: "Service providers listing.",
+      },
+      {
+        src: "/bizzlisting/image copy 4.png",
+        alt: "Beautiful Footer",
+        description: "Footer with Dynamic City data.",
+      }
+    ],
+  },
+  younglearnerslandingpage: {
+    slug: 'younglearnerslandingpage',
+    name: "Young Learners Landing page",
+    image: '/bizzlisting/image.png',
+
+    description:
+      "Figma design for creative educational concept.",
+    longDescription: `
+        Here was my first hands on with Figma, here i learned how to think
+in perspective of end users and design in a well organised way.
+      `,
+    technologies: [
+      "UI",
+      "UX",
+      "Figma"
+    ],
+    links: {
+      repo: "",
+      live: "https://www.figma.com/proto/7rlwhLLorUnX96T2TOqJGY/Aasaan-Project--Copy-?node-id=582-303&starting-point-node-id=582%3A303&t=QRwKfLkAT1KuwssP-1",
+    },
+    dates: "July - August 2023",
+    screenshots: [
+      {
+        src: "/bizzlisting/image copy.png",
+        alt: "Homepage Overview",
+        description:
+          "Clean and modern homepage layout with modern service categories and advertisements.",
+      },
+      {
+        src: "/bizzlisting/image copy 2.png",
+        alt: "Services Page",
+        description:
+          "Detailed services page with filters and service providers.",
+      },
+      {
+        src: "/bizzlisting/image copy 3.png",
+        alt: "Services Page",
+        description: "Service providers listing.",
+      },
+      {
+        src: "/bizzlisting/image copy 4.png",
+        alt: "Beautiful Footer",
+        description: "Footer with Dynamic City data.",
+      }
+    ],
+  },
+  tictactoe: {
+    slug: 'tictactoe',
+    name: "Tic Tac Toe",
+    image: '/bizzlisting/image.png',
+
+    description:
+      "Two player game using LEDs and Pushbuttons",
+    longDescription: `
+        This game uses 3 by 3 matrix of LEDs (Red and Yellow) and
+Pushbuttons. According to player's chance the LED will glow and if
+the player forms the winning pattern the game ends.
+Using waste plastic box with LEDs and Push buttons, utilising the
+small surface area of box with complex wiring structure
+      `,
+    technologies: [
+      "ESP32",
+      "C++",
+      "Arduino",
+      "Electronics"
+    ],
+    links: {
+      repo: "",
+      live: "https://atul.thebigocommunity.org/projects/tictactoe",
+    },
+    dates: "May - June 2023",
+    screenshots: [
+      {
+        src: "/bizzlisting/image copy.png",
+        alt: "Homepage Overview",
+        description:
+          "Clean and modern homepage layout with modern service categories and advertisements.",
+      },
+      {
+        src: "/bizzlisting/image copy 2.png",
+        alt: "Services Page",
+        description:
+          "Detailed services page with filters and service providers.",
+      },
+      {
+        src: "/bizzlisting/image copy 3.png",
+        alt: "Services Page",
+        description: "Service providers listing.",
+      },
+      {
+        src: "/bizzlisting/image copy 4.png",
+        alt: "Beautiful Footer",
+        description: "Footer with Dynamic City data.",
+      }
+    ],
+  },
+  cleanapp2: {
+    slug: 'cleanapp2',
+    name: "Clean App 2.0 featuring Machine Learning",
+    image: '/bizzlisting/image.png',
+
+    description:
+      "Realtime Trash Detection and Location System",
+    longDescription: `
+        Application which utilises individual's camera for real-time trash
+detection and if the garbage is in concerning amount, the app will
+mark the location on map. Users have to place their phone in
+specific area for example like a dash-cam for better view.
+      `,
+    technologies: [
+      "Object Detection",
+      "ML",
+      "Yolo v7",
+      "AI/ML",
+      "Python",
+      "Ultralytics"
+    ],
+    links: {
+      repo: "",
+      live: "https://atul.thebigocommunity.org/projects/thecleanappai",
+    },
+    dates: "March - June 2023",
+    screenshots: [
+      {
+        src: "/bizzlisting/image copy.png",
+        alt: "Homepage Overview",
+        description:
+          "Clean and modern homepage layout with modern service categories and advertisements.",
+      },
+      {
+        src: "/bizzlisting/image copy 2.png",
+        alt: "Services Page",
+        description:
+          "Detailed services page with filters and service providers.",
+      },
+      {
+        src: "/bizzlisting/image copy 3.png",
+        alt: "Services Page",
+        description: "Service providers listing.",
+      },
+      {
+        src: "/bizzlisting/image copy 4.png",
+        alt: "Beautiful Footer",
+        description: "Footer with Dynamic City data.",
+      }
+    ],
+  },
+  cleanapp: {
+    slug: 'cleanapp2',
+    name: "Clean App",
+    image: '/bizzlisting/image.png',
+
+    description:
+      "Location based garbage marking system",
+    longDescription: `
+        Flutter based mobile application for any responsible, environment
+caring individual who would like to report a dumped waste at any
+location around them. A platform where citizens can mark the
+location of openly dumped garbage for concerning authorities to
+deal with.
+      `,
+    technologies: [
+      "Geolocation",
+      "Flutter",
+      "Tech for Environment"
+    ],
+    links: {
+      repo: "",
+      live: "https://atul.thebigocommunity.org/projects/thecleanapp",
+    },
+    dates: "July - August 2022",
+    screenshots: [
+      {
+        src: "/bizzlisting/image copy.png",
+        alt: "Homepage Overview",
+        description:
+          "Clean and modern homepage layout with modern service categories and advertisements.",
+      },
+      {
+        src: "/bizzlisting/image copy 2.png",
+        alt: "Services Page",
+        description:
+          "Detailed services page with filters and service providers.",
+      },
+      {
+        src: "/bizzlisting/image copy 3.png",
+        alt: "Services Page",
+        description: "Service providers listing.",
+      },
+      {
+        src: "/bizzlisting/image copy 4.png",
+        alt: "Beautiful Footer",
+        description: "Footer with Dynamic City data.",
+      }
+    ],
+  }
   // Add more projects here
 };
 
